8d6abecc3bf239f1893813a105e4c29b4391730e
alexis92fra <61412229+alexis92fra@users.noreply.github.com>
2020-02-24 11:49:36 +0100

    Add files via upload

--- pagetest.php
+++ pagetest.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 
 <?php 
-
+session_start();
 $bdd = new PDO('mysql:host=localhost;port:3308;dbname=abcd','root','');
 
-if( isset($_POST['sub2']))
+if( isset($_POST['form2']))
 {
 	$prenom = htmlspecialchars($_POST['prenom']);
 	$nom = htmlspecialchars($_POST['nom']);
@@ -44,6 +44,37 @@ if( isset($_POST['sub2']))
 	}
 }
 
+if(isset($_POST['formconnexion']))
+{
+	$Mailconnect = htmlspecialchars($_POST['Mailconnect']);
+	$mdpconnect = sha1($_POST['mdpconnect']);
+	if (!empty($Mailconnect) AND !empty($mdpconnect)) 
+	{
+		$requser = $bdd->prepare("SELECT * FROM membre WHERE mail = ? AND motdepasse = ?");
+		$requser->execute(array($Mailconnect, $mdpconnect));
+		$userexist = $requser->rowCount();
+		if($userexist == 1)
+		{
+			$userinfo = $requser->fetch();
+			$_SESSION['id'] = $userinfo['id'];
+			$_SESSION['nom'] = $userinfo['nom'];
+			$_SESSION['prenom'] = $userinfo['prenom'];
+			$_SESSION['mail'] = $userinfo['mail'];
+			header("Location: profil.php?id=".$_SESSION['id']);
+
+		}
+		else
+		{
+			$erreur = "Mauvais mail ou mot de passe";
+		}
+	}
+	else
+	{
+		$erreur = "Tous les champs doivent etre compléter !";
+	}
+}
+
+
 ?>
 
 <html>
@@ -54,10 +85,10 @@ if( isset($_POST['sub2']))
 </head>
 <body>
 <header>
-	<form method="post" action="profil.php" name="connexion">
+	<form method="post" name="formconnexion">
 		<div id="base">
-			<p> <label> Adresse e-mail ou mobile </br><input type="email" name="mail" placeholder="Votre Login"><br></label> </p>
-			<p><label>Mot de Passe </br><input type="password" name="pass" placeholder="Votre mot de passe"> <br> <a href="oublie.php">Informations de compte oubliées ? </a></label> </p> <br> <br>
+			<p> <label> Adresse e-mail ou mobile </br><input type="email" name="Mailconnect" placeholder="Votre Login"><br></label> </p>
+			<p><label>Mot de Passe </br><input type="password" name="mdpconnect" placeholder="Votre mot de passe"> <br> <a href="oublie.php">Informations de compte oubliées ? </a></label> </p> <br> <br>
 			<input type="submit" name="submit" value="Connexion" style="padding : 10px, margin: 5px;">
 		</div>
 	</form>
@@ -69,16 +100,16 @@ if( isset($_POST['sub2']))
 		<h1> Inscription </h1>
 		<h2> C'est gratuit (et ça le restera toujours) </h2>
 		<div id="boite2">
-			<p><input type="text" name="prenom" placeholder="Prénom"> </p>
+			<p><input type="text" name="prenom" placeholder="Prénom" required> </p>
 			<p>      </p>
-			<p><input type="text" name="nom" placeholder="Nom de famille"> </p>
+			<p><input type="text" name="nom" placeholder="Nom de famille" required> </p>
 		</div>
-		<p><input type="mail" name="mail1" placeholder="Numéro de mobile ou email"> </p>
-		<p><input type="mail" name="mail2" placeholder="Confirmer numéro de mobile ou email"> </p>
-		<p><input type="password" name="pass" placeholder="Nouveau mot de passe"> </p>
+		<p><input type="mail" name="mail1" placeholder="Numéro de mobile ou email" required> </p>
+		<p><input type="mail" name="mail2" placeholder="Confirmer numéro de mobile ou email" required> </p>
+		<p><input type="password" name="pass" placeholder="Nouveau mot de passe" required> </p>
 		<h4> Date de Naissance </h4>
 		<div id="date">
-			<input type="date" name="date">
+			<input type="date" name="date" required>
 			<a href="explication.php">¨Pourquoi indiquer ma date de naissance</a>
 		</div>
 		<br>
